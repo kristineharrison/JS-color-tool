5565dee290ff3e580182c865de1f6718157215b9
kkochie <kristinekochie@gmail.com>
2022-12-29 10:15:13 -0500

    Display color from user input

--- index.js
+++ index.js
@@ -2,17 +2,26 @@
 // Can start with # and have either 3 or 6 characters
 // Check that input has appropriate type of characters
 
-const validHex = (hex) => {
+const isValidHex = (hex) => {
   if (!hex) return false
 
   const strippedHex =  hex.replace("#","")
   return strippedHex.length === 3 || strippedHex.length === 6
 }
 
+// Get a reference to hexInput and inputColor DOM elements
+// Create a keyup event handler for hexInput
+// Check if hex color is valid
+// If hex color is valid, update the background color of inputColor
 
+const hexInput = document.getElementById("hexInput")
+const inputColor = document.getElementById("inputColor")
 
-console.log(validHex("#334433"))
-console.log(validHex("#3344339"))
-console.log(validHex("fff"))
-console.log(validHex("ac"))
-console.log(validHex("#ac"))
+hexInput.addEventListener('keyup', () => {
+  const hex = hexInput.value
+  if (!isValidHex(hex)) return
+
+  const strippedHex = hex.replace("#", "")
+
+  inputColor.style.backgroundColor = "#" + strippedHex
+})
